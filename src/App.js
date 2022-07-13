72d0006406842e9a2bfad2ed13756b1223b43b22
Paula Cassia Feltrin <93276824+paulafeltrin@users.noreply.github.com>
2022-07-13 08:21:24 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// Crie um Component de class que retorne uma frase na tela
+
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return <h1>Utilizando Component de Class</h1>;
+  }
+}
+
+export default App;
